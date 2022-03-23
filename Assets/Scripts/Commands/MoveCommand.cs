db217959a84b5133f17944fa493fd0fbbc10e429
monaalaa <monabindary@gmail.com>
2022-03-23 14:37:09 +0100

    Move command

---
+++ Assets/Scripts/Commands/MoveCommand.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Commands
+{
+    public class MoveCommand
+    {
+       private readonly Transform _moved;
+       private readonly float _speed;
+       private readonly Vector3 _direction;
+ 
+        public MoveCommand(Transform target, float speed, Vector3 direction)
+        {
+            _moved = target;
+            _speed = speed;
+            _direction = direction;
+        }
+        public void Execute()
+        {
+            _moved.Translate(_direction * _speed * Time.deltaTime);
+        }
+    }
+}
