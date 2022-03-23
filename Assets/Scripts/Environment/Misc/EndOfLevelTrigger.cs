cc6699a6d3c6759df817684e77f36288cdf354e2
monaalaa <monabindary@gmail.com>
2022-03-23 14:36:05 +0100

    trigger end of level

---
+++ Assets/Scripts/Environment/Misc/EndOfLevelTrigger.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Environment.Misc
+{
+    public class EndOfLevelTrigger : MonoBehaviour
+    {
+        private const string BasketTag = "Basket";
+        private void OnTriggerEnter(Collider trigger)
+        {
+            if (trigger.gameObject.CompareTag(BasketTag))
+            {
+                Time.timeScale = 0;
+                EventsManager.GameOver?.Invoke();
+            }
+        }
+    }
+}
