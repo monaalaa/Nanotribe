9d8b2e83b41cad145650420c9e72d6d1f49b514c
monaalaa <monabindary@gmail.com>
2022-03-23 14:36:30 +0100

    Environment System

---
+++ Assets/Scripts/Environment/EnvironmentController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Environment
+{
+    public class EnvironmentController : MonoBehaviour
+    {
+        [SerializeField] private EnvironmentModel model = new EnvironmentModel();
+        private void Awake()
+        {
+            InitRoad();
+        }
+        private void InitRoad()
+        {
+            var index = Random.Range(0,model.roads.Count);
+           Instantiate(model.roads[index],transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/EnvironmentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+    [Serializable]
+    public class EnvironmentModel
+    { 
+        public List<GameObject> roads = new List<GameObject>();
+    }
+}
