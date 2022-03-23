322662c182b39586bf2609258918aa8a102c4b83
monaalaa <monabindary@gmail.com>
2022-03-23 14:19:18 +0100

    Create Basket Model and Controller

---
+++ Assets/Scripts/basket/BasketController.cs
@@ -0,0 +1,61 @@
+﻿using Commands;
+using UnityEngine;
+namespace basket
+{
+    public class BasketController : MonoBehaviour
+    {
+       [SerializeField] private BasketModel data = new BasketModel();
+       private const float YPos = 15.0f;
+       private MoveCommand _command;
+       private void Update()
+       {
+#if !UNITY_EDITOR
+            ExecuteTouch();
+#else
+           ExecuteEditor();
+#endif
+       }
+       private void ExecuteTouch()
+       {
+           if (Input.touchCount > 0)
+           {
+               Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+ 
+               if (touchDeltaPosition.x < 0.0f)
+               {
+                   MoveToLeft();
+               }
+               else if (touchDeltaPosition.x > 0.0f)
+               {
+                   MoveToRight();
+               }
+               var xPos = Mathf.Clamp(transform.localPosition.x, data.leftLimit, data.rightLimit);
+               transform.position = new Vector3(xPos, YPos, 0.0f);
+           }
+       }
+       //Note This function just for testing in editor
+       private void ExecuteEditor()
+       {
+           if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+           {
+               MoveToLeft();
+           }
+           else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+           {
+              MoveToRight();
+           }
+           var xPos = Mathf.Clamp(transform.localPosition.x, data.leftLimit, data.rightLimit);
+           transform.position = new Vector3(xPos, YPos, 0.0f);
+       }
+       private void MoveToRight()
+       {
+           _command = new MoveCommand(transform,data.speed,Vector3.right);
+           _command.Execute();
+       }
+       private void MoveToLeft()
+       {
+           _command = new MoveCommand(transform,data.speed,Vector3.left);
+           _command.Execute();
+       }
+    }
+}
---
+++ Assets/Scripts/basket/BasketModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace basket
+{
+    [Serializable]
+    public class BasketModel
+    {
+        public float rightLimit = 9;
+        public float leftLimit = -9;
+        public float speed = 10;
+    }
+}
